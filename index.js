193df25967ddd0b33c5696cd6fc254becb1981f6
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2022-07-22 17:47:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render('../views/less');
+});
+
+app.listen(3032, function(){
+    console.log('Executando na porta 3032');
+});
+
